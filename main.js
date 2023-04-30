75433ab62619480f1fa8875d936acaca7a1f582d
Cheehan <pockethook@npyu.org>
2023-04-29 19:04:50 -0700

    Prefer smaller boxes during search

--- main.js
+++ main.js
@@ -1,196 +1,201 @@
 "use strict"
 
-function Mouse() {
-	this.active = false;
-	this.x0 = 0;
-	this.y0 = 0;
-	this.x1 = 0;
-	this.y1 = 0;
-	this.begin = function(position) {
-		this.x0 = position.x;
-		this.y0 = position.y;
-	};
-	this.end = function(position) {
-		this.x1 = position.x;
-		this.y1 = position.y;
-	};
-	this.box = function() {
-		return {
-			x: Math.min(this.x0, this.x1),
-			y: Math.min(this.y0, this.y1),
-			width: Math.abs(this.x1 - this.x0),
-			height: Math.abs(this.y1 - this.y0)
-		};
+const Mouse = () => {
+	let active = false;
+	let x0 = 0;
+	let y0 = 0;
+	let x1 = 0;
+	let y1 = 0;
+	return {
+		get_active: () => active,
+		set_active: new_active => {
+			active = new_active;
+		},
+		begin: position => {
+			x0 = position.x;
+			y0 = position.y;
+		},
+		end: position => {
+			x1 = position.x;
+			y1 = position.y;
+		},
+		box: () => {
+			return {
+				x: Math.min(x0, x1),
+				y: Math.min(y0, y1),
+				width: Math.abs(x1 - x0),
+				height: Math.abs(y1 - y0)
+			};
+		},
 	};
 }
 
-function Drawer(canvas_image, canvas_boxes, image) {
-	this.canvas_image = canvas_image;
-	this.canvas_boxes = canvas_boxes;
-	this.context_image = this.canvas_image.getContext("2d");
-	this.context_boxes = this.canvas_boxes.getContext("2d");
-	this.box_colour = 'red';
-	this.box_width = 3;
-	this.lines_colour = 'blue';
-	this.lines_width = 2;
-	this.image = image;
-	this.canvas_image.width = this.image.width;
-	this.canvas_image.height = this.image.height;
-	this.canvas_boxes.width = this.image.width;
-	this.canvas_boxes.height = this.image.height;
-	this.set_image = function(image) {
-		this.image = image;
-		this.canvas_image.width = this.image.width;
-		this.canvas_image.height = this.image.height;
-		this.canvas_boxes.width = this.image.width;
-		this.canvas_boxes.height = this.image.height;
+const Drawer = (canvas_image, canvas_boxes, new_image) => {
+	let image = new_image;
+	const context_image = canvas_image.getContext("2d");
+	const context_boxes = canvas_boxes.getContext("2d");
+	canvas_image.width = image.width;
+	canvas_image.height = image.height;
+	canvas_boxes.width = image.width;
+	canvas_boxes.height = image.height;
+	const box_colour = 'red';
+	const box_width = 3;
+	const lines_colour = 'blue';
+	const lines_width = 2;
+	const pixel_width = () => {
+		return canvas_image.width / image.width;
 	};
-	this.pixel_width = function() {
-		return this.canvas_image.width / this.image.width;
+	const pixel_height = () => {
+		return canvas_image.height / image.height;
 	};
-	this.pixel_height = function() {
-		return this.canvas_image.height / this.image.height;
+	const draw_image = () => {
+		context_image.drawImage(
+			image, 0, 0, image.width, image.height,
+			0, 0, canvas_image.width, canvas_image.height);
 	};
-	this.draw_image = function() {
-		this.context_image.drawImage(
-			this.image, 0, 0, this.image.width, this.image.height,
-			0, 0, this.canvas_image.width, this.canvas_image.height);
+	const draw_box = box => {
+		context_boxes.strokeStyle = box_colour;
+		context_boxes.lineWidth = box_width;
+		context_boxes.strokeRect(
+			box.x * pixel_width(),
+			box.y * pixel_height(),
+			box.width * pixel_width(),
+			box.height * pixel_height());
 	};
-	this.draw_box = function(box) {
-		this.context_boxes.strokeStyle = this.box_colour;
-		this.context_boxes.lineWidth = this.box_width;
-		this.context_boxes.strokeRect(
-			box.x * this.pixel_width(),
-			box.y * this.pixel_height(),
-			box.width * this.pixel_width(),
-			box.height * this.pixel_height());
+	const draw_boxes = annotations => {
+		context_boxes.clearRect(
+			0, 0, canvas_boxes.width, canvas_boxes.height);
+		annotations.forEach(annotation => draw_box(annotation));
 	};
-	this.draw_boxes = function(annotations) {
-		this.context_boxes.clearRect(
-			0, 0, this.canvas_boxes.width, this.canvas_boxes.height);
-		for (var annotation of annotations) {
-			this.draw_box(annotation);
+	const draw_lines = position => {
+		context_boxes.strokeStyle = lines_colour;
+		context_boxes.lineWidth = lines_width;
+		context_boxes.beginPath();
+		context_boxes.moveTo(position.x * pixel_width(), 0);
+		context_boxes.lineTo(
+			position.x * pixel_width(), canvas_boxes.height);
+		context_boxes.stroke();
+		context_boxes.beginPath();
+		context_boxes.moveTo(0, position.y * pixel_height());
+		context_boxes.lineTo(
+			canvas_boxes.width, position.y * pixel_height());
+		context_boxes.stroke();
+	};
+	return {
+		draw_all: (annotations, position) => {
+			draw_image();
+			if (position) {
+				draw_lines(position);
+			}
+			draw_boxes(annotations);
+		},
+		draw_image,
+		draw_boxes,
+		draw_box,
+		draw_lines,
+		set_image: new_image => {
+			image = new_image;
+			canvas_image.width = image.width;
+			canvas_image.height = image.height;
+			canvas_boxes.width = image.width;
+			canvas_boxes.height = image.height;
+		},
+		scale: factor => {
+			canvas_image.width *= factor;
+			canvas_image.height *= factor;
+			canvas_boxes.width *= factor;
+			canvas_boxes.height *= factor;
 		}
 	};
-	this.draw_lines = function(position) {
-		this.context_boxes.strokeStyle = this.lines_colour;
-		this.context_boxes.lineWidth = this.lines_width;
-		this.context_boxes.beginPath();
-		this.context_boxes.moveTo(position.x * this.pixel_width(), 0);
-		this.context_boxes.lineTo(
-			position.x * this.pixel_width(), this.canvas_boxes.height);
-		this.context_boxes.stroke();
-		this.context_boxes.beginPath();
-		this.context_boxes.moveTo(0, position.y * this.pixel_height());
-		this.context_boxes.lineTo(
-			this.canvas_boxes.width, position.y * this.pixel_height());
-		this.context_boxes.stroke();
-	};
-	this.draw_all = function(annotations, position) {
-		this.draw_image();
-		if (position) {
-			this.draw_lines(position);
-		}
-		this.draw_boxes(annotations);
-	};
-	this.scale = function(factor) {
-		this.canvas_image.width *= factor;
-		this.canvas_image.height *= factor;
-		this.canvas_boxes.width *= factor;
-		this.canvas_boxes.height *= factor;
-	};
+};
+
+const point_in_box = (x, y, box) => 
+	x >= box.x && x <= box.x + box.width &&
+	y >= box.y && y <= box.y + box.height;
+
+const box_area = box => box.width * box.height;
+
+const find_best_annotation_index = (annotations, position) => {
+	// Score inverse proportional to box area
+	const scores = annotations.map(annotation =>
+		point_in_box(position.x, position.y, annotation) *
+		(1 / box_area(annotation)));
+	const max_score = Math.max(...scores);
+	return scores.findIndex(score => score && score == max_score);
 }
 
-function point_in_box(x, y, box) {
-	if (x > box.x + box.width || x < box.x ||
-		y > box.y + box.height || y < box.y) {
-		return false;
-	} else { 
-		return true;
-	}
-}
-
-function find_annotation_index(annotations, position) {
-	for (var i = annotations.length - 1; i >= 0; i--) {
-		if (point_in_box(
-			position.x, position.y, annotations[i])) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-function shift_annotations(annotations, x, y) {
-	for (var i = 0; i < annotations.length; i++) {
-		annotations[i].x += x;
-		annotations[i].y += y;
-	}
+const shift_annotations = (annotations, x, y) => {
+	return annotations.map(annotation => ({
+		...annotation,
+		x: annotation.x + x,
+		y: annotation.y + y,
+	}));
 }
 
 document.addEventListener(
 	"DOMContentLoaded",
-	function() {
-		var input = document.getElementById('file-image');
-		var file_annotations = document.getElementById('file-annotations');
+	() => {
+		const input = document.getElementById('file-image');
+		const file_annotations = document.getElementById('file-annotations');
 
-		var canvas_image = document.getElementById('canvas-image');
-		var canvas_boxes = document.getElementById('canvas-boxes');
+		const canvas_image = document.getElementById('canvas-image');
+		const canvas_boxes = document.getElementById('canvas-boxes');
 
-		var save = document.getElementById('save');
-		var download = document.getElementById('download');
+		const save = document.getElementById('save');
+		const download = document.getElementById('download');
 
-		var image = {
+		let image = {
 			name: 'image.jpg',
 			width: window.innerWidth,
 			height: window.innerHeight
 		};
 
-		var drawer = new Drawer(canvas_image, canvas_boxes, image);
-		var annotations = [];
+		const drawer = Drawer(canvas_image, canvas_boxes, image);
+		let annotations = [];
 
-		function load_image(event) {
+		const set_image = event => {
 			image = new Image();
-			image.onload = function() {
+			image.onload = () => {
 				drawer.set_image(image);
-				var position = mouse_position(event);
+				const position = mouse_position(event);
 				drawer.draw_all(annotations, position);
 			};
-			var file = this.files[0];
+			const file = event.target.files[0];
 			image.name = file.name;
 			image.src = window.URL.createObjectURL(file);
 		}
 
-		function load_annotations(event) {
-			var reader = new FileReader();
-			reader.onload = function(event) {
+		const load_annotations = event => {
+			const reader = new FileReader();
+			reader.onload = () => {
 				annotations = JSON.parse(reader.result);
 			};
-			var file = this.files[0];
+			const file = event.target.files[0];
 			reader.readAsText(file, 'utf-8');
 		}
 
-		input.addEventListener('change', load_image);
+		input.addEventListener('change', set_image);
 		file_annotations.addEventListener('change', load_annotations);
 
-		var mouse = new Mouse();
+		const mouse = Mouse();
 
-		function mouse_position(event) {
-			var rect = canvas_boxes.getBoundingClientRect();
-			var canvas_x = event.clientX - rect.left;
-			var canvas_y = event.clientY - rect.top;
+		const mouse_position = event => {
+			const rect = canvas_boxes.getBoundingClientRect();
+			const canvas_x = event.clientX - rect.left;
+			const canvas_y = event.clientY - rect.top;
 			return {
 				x: Math.round(canvas_x * image.width / rect.width),
 				y: Math.round(canvas_y * image.height / rect.height)
 			};
-		}
+		};
 
 		canvas_boxes.addEventListener(
 			'mousedown',
-			function(event) {
-				if (!mouse.active) {
+			event => {
+				if (!mouse.get_active()) {
 					if (event.which == 2 || event.button == 4) {
-						var position = mouse_position(event);
-						var index = find_annotation_index(
+						const position = mouse_position(event);
+						const index = find_best_annotation_index(
 							annotations, position);
 						if (index >= 0) {
 							annotations.splice(index, 1);
@@ -203,13 +208,13 @@ document.addEventListener(
 
 		canvas_boxes.addEventListener(
 			'mousemove',
-			function(event) {
-				var position = mouse_position(event);
+			event => {
+				const position = mouse_position(event);
 				drawer.draw_boxes(annotations);
 				drawer.draw_lines(position);
-				if (mouse.active) {
+				if (mouse.get_active()) {
 					mouse.end(position);
-					var box = mouse.box();
+					const box = mouse.box();
 					drawer.draw_box(box);
 				}
 				
@@ -217,14 +222,14 @@ document.addEventListener(
 
 		canvas_boxes.addEventListener(
 			'click',
-			function(event) {
-				var position = mouse_position(event);
-				if (!mouse.active) {
+			event => {
+				const position = mouse_position(event);
+				if (!mouse.get_active()) {
 					mouse.begin(position);
-					mouse.active = true;
+					mouse.set_active(true);
 				} else {
 					mouse.end(position);
-					mouse.active = false;
+					mouse.set_active(false);
 					annotations.push(mouse.box());
 					drawer.draw_lines(position);
 					drawer.draw_boxes(annotations);
@@ -233,8 +238,8 @@ document.addEventListener(
 
 		save.addEventListener(
 			'click',
-			function(event) {
-				var data = 'data:text/json;charset=utf-8,' +
+			() => {
+				const data = 'data:text/json;charset=utf-8,' +
 					encodeURIComponent(JSON.stringify(annotations));
 				download.setAttribute('href', data);
 				download.setAttribute(
@@ -245,7 +250,7 @@ document.addEventListener(
 
 		window.addEventListener(
 			'keydown',
-			function(event) {
+			event => {
 				switch (event.key) {
 					case 'j':
 						drawer.scale(1.1);
